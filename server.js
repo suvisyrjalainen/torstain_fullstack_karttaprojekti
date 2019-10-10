1142b7f6c23ccf18c2bbd7869ed5792818b5ccc0
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2019-10-10 14:33:15 +0300

    heroku porttifixi

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 
-app.listen(3000, () => console.log("kuuntelen"));
+//app.listen(3000, () => console.log("kuuntelen"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("kuuntelen porttia " + port));
 
 app.use(express.static("public"));
