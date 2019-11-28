6b5ccffd2cae11909d8c7ad749a8d4f2e6fc8694
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2019-11-28 14:46:01 +0200

    sijainnit toimivat

--- public/js/sijainti_ja_kartta.js
+++ public/js/sijainti_ja_kartta.js
@@ -1,23 +1,99 @@
+var latitude = 0;
+var longitude = 0;
+var olet_tassa;
+var kartta;
+
+var paikka_merkitty = 0;
+
+
+async function getData(){
+
+  const response = await fetch('api/paikat');
+  const data = await response.json();
+  console.log(data);
+
+  tayta_paikkataulukko(data);
+  merkitsePaikat(data);
+}
+
+
+function merkitsePaikat(data){
+
+  var kayty_paikka;
+  for (var i = 0; i < data.length; i++) {
+    kayty_paikka = L.marker([data[i].latitude, data[i].longitude]).addTo(kartta);
+    kayty_paikka.bindPopup("<b>" + data[i].paikka + "</b><br>" + data[i].arvostelu).openPopup();
+  }
+
+  if(paikka_merkitty == 0){
+      olet_tassa = L.marker([latitude, longitude]).addTo(kartta);
+      olet_tassa.bindPopup("<b>Olet Tässä</b><br>" + "haluatko merkitä paikan?").openPopup();
+  }
+}
+
+function tyhjenna_paikkataulukko(){
+  var table = document.getElementById("paikkataulukko");
+
+  console.log(table);
+  //document.getElementById("paikkataulukkobody").innerHTML = "";
+  console.log("Paikkataulukko tyhjataan");
+
+  var rivien_maara = table.rows.length - 1;
+  console.log(rivien_maara);
+
+  for (var i = 0; i < rivien_maara; i++) {
+      console.log("Paikka deletoitu" + i);
+      table.deleteRow(1);
+      console.log(table);
+  }
+
+  console.log(table);
+
+}
+
+function tayta_paikkataulukko(data){
+  var table = document.getElementById("paikkataulukko");
+
+  for (var i = 0; i < data.length; i++) {
+    var row = table.insertRow(i + 1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = data[i].paikka;
+    cell2.innerHTML = data[i].arvostelu;
+  }
+}
+
+
+
 if("geolocation" in navigator) {
   console.log("Sijaintieto saatavilla");
   navigator.geolocation.getCurrentPosition(function(position) {
       console.log(position);
 
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
 
       console.log(latitude);
       console.log(longitude);
       document.getElementById("leveysasteet").innerHTML = latitude;
       document.getElementById("pituusasteet").innerHTML = longitude;
 
-      var kartta = L.map('kartta').setView([latitude, longitude], 13);
+      kartta = L.map('kartta').setView([latitude, longitude], 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(kartta);
 
-      var marker = L.marker([latitude, longitude]).addTo(kartta);
+      getData();
+
+
+
+      //kartta.panTo(new L.LatLng(latit, longit));
+      //kartta.panTo(olet_tassa.getLatLng());
+
+
+
+
   });
 }
 else{
@@ -40,4 +116,29 @@ function laheta_arvostelu(){
   console.log(paikka);
   console.log(arvostelu);
 
+  const data = {latitude, longitude, paikka, arvostelu};
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+
+  fetch('/api/arvostelu', options).then(function(response) {
+      console.log(response)
+      if(response.status == 200){
+        tyhjenna_paikkataulukko();
+        getData();
+        sulje_paikkatietolomake();
+        paikka_merkitty = 1;
+        //kartta.removeLayer(olet_tassa);
+        //olet_tassa = L.marker([latitude, longitude]).addTo(kartta);
+        //olet_tassa.bindPopup("<b>" + paikka + "</b><br>" + arvostelu).openPopup();
+
+      }
+    }, function(error){
+      console.log(error.message);
+    });
 }
--- server.js
+++ server.js
@@ -10,8 +10,31 @@ app.use(express.static("public"));
 app.use(express.json({limit: '1mb'}));
 
 
+const paikat = [
+    {
+        "paikka": "Helsinki",
+        "arvostelu": "Kivaa oli",
+        "longitude": "24.9384",
+        "latitude": "60.1699"
+
+    },
+    {
+      "paikka": "Levi",
+      "arvostelu": "Hyvat laskusaat",
+      "longitude": "24.8082",
+      "latitude": "67.8040"
+    }
+]
+
+app.get('/api/paikat', function (request, response) {
+  response.send(paikat);
+})
+
+
 app.post('/api/arvostelu', function (request, response) {
   console.log("Kayttajan arvostelu");
   console.log(request.body);
+  paikat.push(request.body);
+  console.log(paikat);
   response.send(200);
 })
